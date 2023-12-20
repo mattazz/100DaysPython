2fea31de6440d33d3269a506aa0339d9db86df85
Matthew Azada <matthewazada101@gmail.com>
2023-12-20 13:02:41 -0500

    finished exercise

--- Day5/average_height.py
+++ Day5/average_height.py
@@ -1,1 +1,23 @@
 # Average height exercise
+
+student_heights = [180, 124, 165, 173, 189, 169, 146]
+
+
+def loop_ave():
+    ave = 0
+    for i in student_heights:
+        ave += i
+
+    res = ave / len(student_heights)
+    res = round(res, 2)
+    print(f"The average student height is {res}")
+
+
+def other_answer():
+    res = sum(student_heights) / len(student_heights)
+    res = round(res, 2)
+    print(f"Alternative answer: {res}")
+
+
+loop_ave()
+other_answer()
