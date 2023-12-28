db483d21fd3169a736ee1dc433b9d2d27028ec3b
Matthew Azada <matthewazada101@gmail.com>
2023-12-28 13:33:00 -0500

    hangman game

--- Day7/guess_letter.py
+++ Day7/guess_letter.py
@@ -1,31 +1,60 @@
 import random
 
-while True:
-    try:
-        letter = str(input("Guess a letter: "))
-        if len(letter) > 1:
-            print("Too many letters")
-            continue
-        elif letter.isdigit():
-            print("Cannot be a number")
-            continue
-        else:
-            break
-    except:
-        print("Invalid input")
-
-print(f"You chose: {letter}")
+player_life = 3
 
 
-def check_words(letter):
+def check_words():
+    global player_life
+
     words_list = ["ardvark", "baboon", "camel"]
     chosen = words_list[random.randrange(0, len(words_list))]
 
-    for char in chosen:
-        if letter == char:
-            print("Right")
-        else:
-            print("Wrong")
+    # Create a list from the chosen word
+    blanks = ["_" for i in chosen]
+    print(blanks)
+
+    while True:
+        print(f"Player life: {player_life}")
+        while True:
+            letter = str(input("Guess a letter: "))
+            if letter == "exit":
+                print("Exiting game...")
+                exit()
+            try:
+                if len(letter) > 1:
+                    print("Too many letters")
+                    continue
+                elif letter.isdigit():
+                    print("Cannot be a number")
+                    continue
+                else:
+                    break
+            except:
+                print("Invalid input")
+
+        print(f"You chose: {letter}")
+
+        guessed_correctly = False
+        for i, char in enumerate(chosen):
+            if letter == char:
+                print("Right")
+                blanks[i] = char
+                guessed_correctly = True
+            else:
+                print("Wrong")
+
+        if player_life <= 0:
+            print("You lose!")
+            exit()
+
+        if not guessed_correctly:
+            player_life -= 1
+
+        print(blanks)
+        if "".join(blanks) == chosen:
+            print(f"The word is {chosen}. You win!")
+            print("You win")
+            break
 
 
-check_words(letter)
+check_words()
